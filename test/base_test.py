f1dda6fbc7b8f39f1e4ef852bfeae56ef50f8c28
izo30 <isaacwangethi30@gmail.com>
2019-04-05 19:01:59 +0300

    [Chore #165116323] Write tests

--- test/base_test.py
+++ test/base_test.py
@@ -1,4 +1,26 @@
 
 # contains configuration for tests
+from app import create_app
 
 from unittest import TestCase
+import os
+import json
+
+from instance.db_config import DbSetup
+
+
+class BaseTest(TestCase):
+
+    def setUp(self):
+        self.app = create_app('testing')
+        self.client = self.app.test_client
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+        DbSetup().create_categories_table()
+        DbSetup().create_customers_table()
+        DbSetup().create_groceries_table()
+
+    def tearDown(self):
+        DbSetup().drop_tables()
+        self.app_context.pop()
---
+++ test/test_categories.py
@@ -0,0 +1,21 @@
+# perform tests for grocery categories
+#  1. add category
+#  2. get categories
+#  3. delete category
+
+from test.base_test import BaseTest
+
+
+class TestCategories(BaseTest):
+
+    # add category
+    def test_add_category(self):
+        pass
+
+    #  get categories
+    def test_get_categories(self):
+        pass
+
+    # delete categories
+    def test_delete_category(self):
+        pass
