eabecedfd53a74c60bb145c162343b226216d5d7
izo30 <isaacwangethi30@gmail.com>
2019-04-06 22:38:22 +0300

    [Feature #165163707] Add login endpoint

--- app/api/utils/fields.py
+++ app/api/utils/fields.py
@@ -13,12 +13,12 @@ customer_api = Namespace('Customer Endpoints', description='A collection of\
 
 class CustomerFields():
     @staticmethod
-    def signup_parser():
+    def signup_args():
         parser = reqparse.RequestParser()
         Validations().add_arguments(parser, ['first_name', 'last_name',
-                                             'email', 'phone', 'location', 
+                                             'email', 'phone', 'location',
                                              'password', 'role'])
-        return parser
+        return parser.parse_args()
 
     signup_fields = customer_api.model('Signup', {
         'first_name': fields.String,
@@ -26,6 +26,18 @@ class CustomerFields():
         'email': fields.String,
         'phone': fields.String,
         'location': fields.String,
+        'password': fields.String,
+        'role': fields.String
+    })
+
+    @staticmethod
+    def login_args():
+        parser = reqparse.RequestParser()
+        Validations().add_arguments(parser, ['email', 'password', 'role'])
+        return parser.parse_args()
+
+    login_fields = customer_api.model('Login', {
+        'email': fields.String,
         'password': fields.String,
         'role': fields.String
     })
