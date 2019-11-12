aa2d986a2d6b0223bc626b149e8ddf9455a9444a
izo30 <isaacwangethi30@gmail.com>
2019-11-12 12:37:02 +0300

    [Feature #165957063] Delete category

--- app/api/views/category_views.py
+++ app/api/views/category_views.py
@@ -1,4 +1,4 @@
-#  contain views for categories. Include the follwoing:
+#  contain views for categories. Include the following:
 #  1. post a category
 #  2. get all categories
 #  3. delete a category
@@ -8,7 +8,6 @@ from flask_restplus import Resource
 from app.api.models.category_model import Category
 from app.api.utils.validations import Validations
 from app.api.utils.auth import Authorization, admin_required
-from app.api.utils.validations import Validations
 from app.api.utils.fields import categories_api as api, CategoriesFields
 
 
@@ -47,3 +46,44 @@ class CategoryViews(Resource):
                 'status': 'Fail',
                 'error': 'Category already exists'
             }, 403
+
+    def get(self):
+        """get all categories"""
+        categories = Category().retrieve_all_categories()
+
+        if categories:
+            return {
+                'status': 'Success',
+                'categories': categories
+            }, 200
+        else:
+            return {
+                'status': 'Fail',
+                'error': 'No category found'
+            }, 404
+
+    @api.expect(CategoriesFields.delete_category_fields)
+    @api.doc(security='apikey')
+    @admin_required
+    def delete(self):
+        """delete an existing category"""
+
+        args = CategoriesFields.delete_args()
+        id = args['id']
+
+        if not id:
+            return {
+                'status': 'Fail',
+                'error': 'ID should not be empty'
+            }, 400
+
+        if Category().delete_category(id):
+            return {
+                'status': 'Success',
+                'message': 'Deleted successfully'
+            }, 201
+        else:
+            return {
+                'status': 'Fail',
+                'error': 'Category cannot be deleted or does not exist'
+            }, 404
